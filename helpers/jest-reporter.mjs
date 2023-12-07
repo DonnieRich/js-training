acb7ac8e5478f99d47622c124284be05feb6a66f
Donnie <donniepask@gmail.com>
2023-12-07 20:20:19 +0100

    Added tests results and final report

--- helpers/jest-reporter.mjs
+++ helpers/jest-reporter.mjs
@@ -24,24 +24,16 @@ export default class CustomReporterWrapper {
 
         if (!testResults.skipped) {
             results = testResults.testResults.map(result => {
-
                 let reasons = "";
-                result.failureMessages.forEach(message => {
 
-                    let expected = message.match(/Expected.*/);
-                    let received = message.match(/Received.*/)
-
-                    if (expected) {
-                        reasons += `${expected}\n`;
-                    }
-
-                    if (received) {
-                        reasons += `${received}\n`;
-                    }
-
+                result.failureDetails.forEach(detail => {
+                    const matcherResult = detail.matcherResult;
+                    const expected = chalk.green(JSON.stringify(matcherResult.expected));
+                    const actual = chalk.red(JSON.stringify(matcherResult.actual));
+                    reasons += `Expected: ${expected}\nReceived: ${actual}\nFailing to assert ${expected} ${matcherResult.name} ${actual}\n`;
                 });
 
-                let message = `${result.title} ${result.status.toUpperCase()}\n`;
+                let message = `${result.title} ${result.status.toUpperCase()} \n`;
 
                 if (result.status === "failed") {
                     message = chalk.bgRed.bold(message);
@@ -60,5 +52,27 @@ export default class CustomReporterWrapper {
     }
 
     onRunComplete(test, runResults) {
+        const totalTests = runResults.numFailedTests + runResults.numPassedTests;
+        const failed = runResults.numFailedTests;
+        const passed = runResults.numPassedTests;
+        const successPercentage = Math.round((passed / totalTests) * 100);
+
+        if (failed) {
+            log(chalk.red(`Failed ${failed} test${failed > 1 ? "s" : ""}`));
+        }
+
+        if (passed) {
+            const total = passed === totalTests ? "all" : passed;
+            log(chalk.green(`Passed ${total} test${passed > 1 ? "s" : ""}`))
+        }
+
+        if (successPercentage < 60) {
+            log(chalk.red(`${successPercentage}% of tests passed...`));
+        } else if (successPercentage < 90) {
+            log(chalk.yellow(`${successPercentage}% of tests passed...`));
+        } else {
+            log(chalk.green(`${successPercentage}% of tests passed...`));
+        }
+
     }
 }
