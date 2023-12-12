66214fcc320a329eb6fb7285afb17c65016a49ae
Donnie <donniepask@gmail.com>
2023-12-12 19:21:02 +0100

    Setting up the command for test scaffolding, minor text fixes

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 #! /usr/bin/env node
 
 import { Command } from "commander";
-import { translateName } from "./helpers/name-converter.mjs";
-import snacks from "./helpers/snack-export.mjs";
+import { translateName } from "./helpers/name-converter.js";
+import snacks from "./helpers/snack-export.js";
 
 const program = new Command();
 
 program
     .option('-i, --info-snack <name>', 'Get info for the selected snack')
+    .option('-c, --create-snack <name>', 'Create the scaffolding for a new snack')
     .option('-h --help', 'To use this package you have to read the info for the snack you want to solve, add your code and then run the test for that snack');
 
 program.parse(process.argv);
@@ -23,11 +24,14 @@ if (options.infoSnack) {
     npm run test -- ${snackName}
     `);
 }
+if (options.createSnack) {
+
+}
 if (options.help) {
     console.log(`
     To use this package you have to read the info for the snack you want to solve, add your code and then run the test for that snack.
     For example:
-        - open snacks/snack-1.mjs
+        - open snacks/easy/multi-sum/snack.js
         - read carefully the instructions with the input/output instructions
         - add your code in the solution function (NOTE: the function should return your solution)
         - run the corresponding test and check if your solution pass all the test cases
