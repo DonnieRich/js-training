935baca6771f4116dbef900f840f3ee352505b26
Donnie <donniepask@gmail.com>
2023-12-26 16:44:53 +0100

    Improved hard string-compactor solution, added comments

--- snacks/hard/string-compactor/.solution/snack.js
+++ snacks/hard/string-compactor/.solution/snack.js
@@ -1,25 +1,40 @@
 
 const solution = (s) => {
-    const stringAsArray = s.split('');
+    // First we save the length of the string (it's just a little optimization)
+    // In this way we will avoid computing the string (or array) length at every iteration
+    const stringLength = s.length;
     const result = []
 
-    for (const letter of stringAsArray) {
+    // Since we can access every character in the string using square brackets notation (an alternative is the charAt() method)
+    // We will use a for loop to iterate through the string
+    for (let i = 0; i < stringLength; i++) {
+
+        // A valid alternative is:
+        // const letter = s.charAt(i)
+        const letter = s[i];
+
+        // We get the last item in the result array (i.e. the current letter we are counting)
+        // If result is empty, the at() method will return undefined
         const lastItem = result.at(-1);
+
+        // If the current letter equals the previous saved letter, we increment the count property
         if (lastItem && lastItem.letter === letter) {
             lastItem.count++;
         } else {
-            const prevItem = result.at(-1);
+
+            // Otherwise we encountered a new letter, so we need to add a new object in the array
             const newItem = {
                 letter: letter,
-                count: 1,
-                position: prevItem ? prevItem.position + 1 : 1
+                count: 1
             };
             result.push(newItem);
         }
     }
 
-    result.sort((a, b) => a.position - b.position);
-
+    // Once we completed the iterations on the string we use the map method to create a new array
+    // The new array will be an array of strings (the letter plus the counter)
+    // Then we'll concatenate all the strings together (the join() method with an empty string as parameter will concatenate all the elements in the array
+    // without spaces or other characters)
     return result.map(l => `${l.letter}${l.count}`).join('');
 };
 
